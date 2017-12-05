eaa39c798e870baaa911f5e8d798ebd736f8b7f0
LouiseReid <l_reid@hotmail.co.uk>
2017-12-05 19:31:22 +0000

    initial commit

---
+++ public/app.js
@@ -0,0 +1,26 @@
+var initialise = function(){
+  var container = document.getElementById('main-map');
+  var center = {lat: 55.864237, lng: -4.251806};
+  var zoom = 16
+  var mainMap = new MapWrapper(container, center, zoom);
+  mainMap.addMarker(center);
+  mainMap.addClickEvent()
+
+
+  var georgeSq = {lat: 55.861152, lng: -4.250196};
+  mainMap.addMarker(georgeSq);
+  mainMap.addInfoWindow();
+
+
+  var bounceButton = document.getElementById('button-bounce-markers');
+  bounceButton.addEventListener('click', mainMap.bounceMarkers.bind(mainMap));
+
+  var chicago = document.getElementById('chicago');
+  chicago.addEventListener('click', mainMap.chicago.bind(mainMap));
+
+  var location = document.getElementById('locate');
+  location.addEventListener('click', mainMap.location.bind(mainMap));
+
+}
+
+window.addEventListener('load', initialise);
---
+++ public/mapWrapper.js
@@ -0,0 +1,47 @@
+var MapWrapper = function(container, coords, zoom){
+  this.googleMap = new google.maps.Map(container, {
+    center: coords,
+    zoom: zoom
+  });
+  this.markers = [];
+}
+
+MapWrapper.prototype.addMarker = function(coords){
+  var marker = new google.maps.Marker({
+    position: coords,
+    map: this.googleMap
+  });
+  this.markers.push(marker);
+}
+
+MapWrapper.prototype.addClickEvent = function(){
+  google.maps.event.addListener(this.googleMap, 'click', function(event){
+    this.addMarker({lat: event.latLng.lat(), lng: event.latLng.lng()})
+  }.bind(this));
+}
+
+MapWrapper.prototype.bounceMarkers = function(){
+  this.markers.forEach(function(marker){
+    marker.setAnimation(google.maps.Animation.BOUNCE)
+  })
+}
+
+MapWrapper.prototype.addInfoWindow = function(){
+  var infoWindow = new google.maps.InfoWindow({
+    content: "This is george square, an ice rink might be here",
+  });
+  var marker = this.markers[1];
+  infoWindow.open(this.googleMap, marker)
+}
+
+MapWrapper.prototype.chicago = function(){
+  this.googleMap.setCenter({lat: 41.878114, lng: -87.629798})
+}
+
+MapWrapper.prototype.location = function(){
+  navigator.geolocation.getCurrentPosition(function(position){
+    var lat = position.coords.latitude;
+    var lng = position.coords.longitude;
+    this.googleMap.setCenter({lat, lng});
+  }.bind(this));
+}
